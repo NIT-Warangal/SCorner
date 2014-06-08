075bc8baf7b16a2a9310082da715e752d1cf22f9
sudheesh001 <sudheesh1995@outlook.com>
2014-06-08 19:04:28 +0300

    Adding all 4 cases test logins to database

--- Database.sql
+++ Database.sql
@@ -17,7 +17,13 @@ CREATE TABLE IF NOT EXISTS `Login` (
   PRIMARY KEY (SNo)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Standard Login values for testing different environments.
+
 INSERT INTO Login VALUES (1,'12345','root',MD5('root'),1);
+INSERT INTO Login VALUES (2,'67890','studentroot',MD5('studentroot'),2);
+INSERT INTO Login VALUES (3,'34567','profroot',MD5('profroot'),3);
+INSERT INTO Login VALUES (4,'24689','modroot',MD5('modroot'),4);
+
 -- Short Anonymous Posts. Used for ranting about problems.
 
 CREATE TABLE IF NOT EXISTS `AnonymousPosts` (
