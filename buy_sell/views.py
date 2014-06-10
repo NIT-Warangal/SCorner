15332bf610506e0583c7970696beea909a62213b
sudheesh001 <sudheesh1995@outlook.com>
2014-06-10 19:22:49 +0300

    Respecting the spacing

--- buy_sell/views.py
+++ buy_sell/views.py
@@ -8,12 +8,14 @@ mysql.init_app(app1)
 for key in config:
     app1.config[key] = config[key]
 app1.config.from_object(__name__)
+
 def get_cursor():
     return mysql.connect().cursor()
 
 @app1.route('/')
 def index():
     return render_template('index.html')
+
 @app1.route('/store')
 def store():
 	db=get_cursor()
@@ -21,36 +23,28 @@ def store():
 	db.execute(sql)
 	entries=db.fetchall()
 	uploader=[]
-	category=[]
 	for entry in entries:
 		sql='select UserName from login where RollNo="%s"'%(entry[0])
 		db.execute(sql)
 		uploader.append(db.fetchone())
 		db.execute("commit")
-		sql='select CategoryName from store_categories where categoryid=%s'%(entry[2])
-		db.execute(sql)
-		category.append(db.fetchone())
-	db.execute('select distinct CategoryID,CategoryName from store_categories')		
-	filter_cat=db.fetchall()
-	return render_template('store.html',entries=entries,uploader=uploader,category=category,filter_cat=filter_cat)
+	db.execute('select distinct Category from store')		
+	category=db.fetchall()
+	return render_template('store.html',entries=entries,uploader=uploader,category=category)
 
 @app1.route('/filter',methods=['POST'])
 def filter():
 	db=get_cursor()
 	category=request.form['filter']
-	sql='select * from store where categoryid="%s"'%(category)
+	sql='select * from store where category="%s"'%(category)
 	db.execute(sql)
 	entries=db.fetchall()
 	uploader=[]
-	category=[]
 	for entry in entries:
 		sql='select UserName from login where RollNo="%s"'%(entry[0])
 		db.execute(sql)
 		uploader.append(db.fetchone())
 		db.execute("commit")
-		sql='select CategoryName from store_categories where categoryid=%s'%(entry[2])
-		db.execute(sql)
-		category.append(db.fetchone())
-	db.execute('select distinct CategoryID,CategoryName from store_categories')		
-	filter_cat=db.fetchall()
-	return render_template('store.html',entries=entries,uploader=uploader,category=category,filter_cat=filter_cat)
+	db.execute('select distinct Category from store')		
+	category=db.fetchall()
+	return render_template('store.html',entries=entries,uploader=uploader,category=category)
