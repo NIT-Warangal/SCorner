5724fb9f0d0d8cf3af7edc1290a4b02a2012c044
kranthikiran01 <guduru.kranthikiran@gmail.com>
2014-06-17 00:42:50 +0530

    Pagination bug fixed #9

--- server.py
+++ server.py
@@ -63,6 +63,7 @@ def login():
 	error = None
 	db = get_cursor()
 	session['temp']=0
+	session.permanent = True
 	if request.method=='POST':
 		uname=str(request.form['username'])
 		pwd=str(request.form['password'])
@@ -189,7 +190,14 @@ def shout():
 	except ValueError:
 		page = 1
 	db = get_cursor()
-	sql = 'select * from AnonymousPosts order by Date desc'
+	start=0
+	per_page=3
+	if page==1:
+		start=0
+	else:
+		start=(page-1)*per_page
+	sql = 'select * from AnonymousPosts order by Date desc limit %s,%s'%(start,per_page)
+	print sql
 	db.execute(sql)
 	posts = db.fetchall()
 	db.execute("commit")
@@ -206,7 +214,11 @@ def shout():
 			like=int(result[0])
 		activity.append(int(like))
 		i=i+1
-	pagination = Pagination(page = page ,per_page=1,total = i, search=search,record_name = 'posts',bs_version=3)
+	query='select Count(*) from anonymousposts'
+	db.execute(query)
+	total=int(db.fetchone()[0])
+	db.execute("commit")
+	pagination = Pagination(page = page ,per_page=per_page,total = total, search=search,record_name = 'posts',bs_version=3)
 	return render_template('shout/screen.html',posts=posts,UName=app.config['USERNAME'],activity=activity,pagination=pagination) #show_entries
 #---------------Buy_Sell---------------
 
